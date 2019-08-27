1e1dcaa3a66a389593f80650d4f053a20a9c4cba
Klaykap <43180618+Klaykap@users.noreply.github.com>
2019-08-27 19:49:33 +0200

    Update lightningos.c

--- source/lightningos.c
+++ source/lightningos.c
@@ -1,6 +1,13 @@
 //LIGHTNINGOS
 
-#define MEMORY_MOUSE 0x100000
+#define MOUSE_MEMORY 0x100000
+#define FREE_JUS_MEMORY 0x200000
+#define FBN_MEMORY 0x300000
+#define DESCRIPTION_MEMORY 0x600000
+
+#define STARTING 1
+#define DESKTOP 2
+#define MENU 3
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
@@ -10,6 +17,8 @@ typedef unsigned long long uint64_t;
 uint32_t mouse_cursor_visible=0;
 uint32_t mouse_x=0;
 uint32_t mouse_y=0;
+
+uint32_t program=0;
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
@@ -49,19 +58,31 @@ static inline void outl(uint16_t port, uint32_t val) {
 
 #include "drivers/vesa.c"
 
+#include "drivers/pc_speaker.c"
+
 #include "drivers/keyboard.c"
 #include "drivers/mouse.c"
+#include "drivers/ata.c"
+
+#include "drivers/jus.c"
 
 #include "drivers/handlers.c"
 #include "drivers/idt.c"
 #include "drivers/time.c"
 #include "drivers/pci.c"
 
+#include "drivers/rtl8139.c"
+
+#include "drivers/usb_uhci.c"
+
+#include "gui/elements.c"
+#include "gui/desktop.c"
 #include "gui/main.c"
  
 void start_lightningos(void) {
+	program = STARTING;
+
 	print_line = 0;
-    uint16_t *mouse_memory = (uint16_t *) 0x100000;
 
 	mouse_cursor_visible=0;
 
@@ -77,6 +98,7 @@ void start_lightningos(void) {
     p("Linear frame buffer:");
     print_hex(vesa_lfb, 1, 20, BLACK);
 
+	p("Setting debug values...");
 	debug_line=10;
 	debug_column=0;
 
@@ -87,12 +109,22 @@ void start_lightningos(void) {
     set_idt();
 	p("Interrupts is enabled");
 
-	p("Mouse initalizing...");
+	p("Choosing ATA drive...");
+	outb(0x1F6, 0x40);
+	if(inb(0x1F7) & 0x01) {
+		outb(0x1F6, 0x40);
+	}
+
+	p("Loading JUS fbn informations...");
+	load_fbn();
+
+	p("Initalizing mouse...");
 	mouse_initalize();
 	mouse_cycle=0;
-    clear_mouse_memory();
+    scan_monitor(0, 0, 16, 16, MOUSE_MEMORY);
 	mouse_x=0;
 	mouse_y=0;
+	handle_irq12=0;
 	mouse_cursor_visible=1;
 
 	p("Starting GUI...");
