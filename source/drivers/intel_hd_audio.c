11d31f5904a2312d970861f8bc0edea66e68ea0f
Klaykap <43180618+Klaykap@users.noreply.github.com>
2019-09-24 18:58:49 +0200

    Update intel_hd_audio.c

--- source/drivers/intel_hd_audio.c
+++ source/drivers/intel_hd_audio.c
@@ -1,5 +1,7 @@
 //LIGHTNINGOS
 
+uint32_t corb_pointer=0;
+
 void hda_outb(uint32_t offset, uint8_t value) {
 	uint8_t *mem = (uint8_t *) (hda_base+offset);
 	mem[0]=value;
@@ -30,34 +32,45 @@ uint32_t hda_inl(uint32_t offset) {
 	return (mem[0]);
 }
 
-void hda(void) {
-	uint32_t *corb = (uint32_t *) 0x00700000;
-	uint32_t *rirb = (uint32_t *) 0x00800000;
+void corb_write_command(uint32_t cad, uint32_t nid, uint32_t verb, uint32_t command) {
+	uint32_t *corb = (uint32_t *) CORB_MEMORY;
 
-	hda_outl(0x40, 0x00700000);  //CORB buffer
-	hda_outl(0x50, 0x00800000);  //RIRB buffer
+	corb[corb_pointer]=( (cad << 28) | (nid << 20) | (verb << 8) | (command) );
+
+	corb_pointer++;
+}
+
+void hda(void) {
+	uint32_t *corb = (uint32_t *) CORB_MEMORY;
+	uint32_t *rirb = (uint32_t *) RIRB_MEMORY;
+
+	hda_outl(0x40, CORB_MEMORY);  //CORB buffer
+	hda_outl(0x50, RIRB_MEMORY);  //RIRB buffer
 	hda_outl(0x44, 0x00000000);  //CORB buffer
 	hda_outl(0x54, 0x00000000);  //RIRB buffer
 
 	//write commands into CORB
-	corb[0]=0x04F00000;
+	for(int i=0; i<16; i++) {
+		corb_write_command(i, 0, 0xF00, 0);
+	}
 
-	hda_outw(0x48, 1); //number of commands to corb
+	hda_outw(0x48, 16); //number of commands to corb
 	hda_outw(0x4A, 0x0080); //reset corb
 
+	wait();
 	wait();
 
 	hda_outw(0x4A, 0x0000); //reset corb
 
-	hda_outw(0x58, 1); //number of commands to rirb
+	hda_outw(0x58, 16); //number of commands to rirb
 
 	hda_outb(0x5C, (hda_inb(0x5C) | 2));
 	hda_outb(0x4C, (hda_inb(0x4C) | 2));
 
 	wait();
+	wait();
 
-	pv(rirb[0]);
-	pv(rirb[1]);
-	pv(rirb[2]);
-	pv(rirb[3]);
+	for(int i=0; i<20; i++) {
+		pv(rirb[i]);
+	}
 }
