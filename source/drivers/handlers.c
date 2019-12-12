83fdbda568e1e2bf3c8a2ba2b14eb4968e92d734
Klaykap <43180618+Klaykap@users.noreply.github.com>
2019-12-12 17:58:47 +0100

    Update handlers.c

--- source/drivers/handlers.c
+++ source/drivers/handlers.c
@@ -1,7 +1,16 @@
 //LIGHTNINGOS
 
-uint32_t mouse_bytes[2];
-uint8_t mouse_cycle=0;
+uint8_t mouse_packet[3];
+
+uint32_t mouse_cycle=0;
+uint32_t debug_line=0;
+uint32_t debug_column=0;
+
+uint32_t handle_irq12=0;
+
+uint32_t wait_var=0;
+
+uint32_t time_cycle=0;
 
 #define MOUSE_NO_BUTTON 0
 #define MOUSE_LEFT 1
@@ -12,46 +21,51 @@ uint8_t mouse_cycle=0;
 #define MOUSE_SCROLL_UP 1
 #define MOUSE_SCROLL_DOWN 2
 
-uint32_t debug_line=0;
-uint32_t debug_column=0;
-void debug_irq1(uint8_t key, uint8_t keycode) {
-	uint8_t message[2];
+void key_is_press(uint8_t keycode, uint8_t ascii);
+void draw_time(void);
+void hda_irq(void);
 
-	if(keycode==KEY_ENTER) {
-		debug_line++;
-		debug_column=0;
-	}
-	else {
-		message[0]=key;
-		message[1]=0;
+void wait(void) {
+	uint32_t tmp=0;
 
-		print(message, debug_line, debug_column, BLACK);
+	tmp=wait_var;
 
-		if(debug_column==99) {
-			debug_line++;
-			debug_column=0;
-		}
-		else {
-			debug_column++;
+	for(int i=0; i<100; i++) {
+		asm("hlt");
+		if(wait_var!=tmp) {
+			return;
 		}
 	}
 }
 
+void isr_handler(void) {
+	p("ISR!");
+}
+
 void irq0_handler(void) {
+	if(wait_var==0) {
+		wait_var=1;
+	}
+	else {
+		wait_var=0;
+	}
+
+	time_cycle++;
+	if(time_cycle>=10 && program!=STARTING) {
+		draw_time();
+		time_cycle=0;
+	}
+
     outb(0x20, 0x20); //EOI
 }
 
 void irq1_handler(void) {
-    outb(0x20, 0x20); //EOI
-
     if (inb(0x64) & 0x01) {
         keycode = inb(0x60);
 
-		/*if(keycode==KEY_SPACE) {
-			pv(mouse_x);
-			pv(mouse_y);
-			pv(mouse_cycle);
-		}*/
+		if(keycode==KEY_F10) {
+			mouse_cycle=0;
+		}
 
         if(keycode==KEY_CAPSLOCK) {
             if(shift==0) {
@@ -82,55 +96,57 @@ void irq1_handler(void) {
         
         //convert keycode to ascii
         key_ascii=0;
-    if(keycode==KEY_A) { key_ascii = 'a'; }
-    if(keycode==KEY_B) { key_ascii = 'b'; }
-    if(keycode==KEY_C) { key_ascii = 'c'; }
-    if(keycode==KEY_D) { key_ascii = 'd'; }
-    if(keycode==KEY_E) { key_ascii = 'e'; }
-    if(keycode==KEY_F) { key_ascii = 'f'; }
-    if(keycode==KEY_G) { key_ascii = 'g'; }
-    if(keycode==KEY_H) { key_ascii = 'h'; }
-    if(keycode==KEY_I) { key_ascii = 'i'; }
-    if(keycode==KEY_J) { key_ascii = 'j'; }
-    if(keycode==KEY_K) { key_ascii = 'k'; }
-    if(keycode==KEY_L) { key_ascii = 'l'; }
-    if(keycode==KEY_M) { key_ascii = 'm'; }
-    if(keycode==KEY_N) { key_ascii = 'n'; }
-    if(keycode==KEY_O) { key_ascii = 'o'; }
-    if(keycode==KEY_P) { key_ascii = 'p'; }
-    if(keycode==KEY_Q) { key_ascii = 'q'; }
-    if(keycode==KEY_R) { key_ascii = 'r'; }
-    if(keycode==KEY_S) { key_ascii = 's'; }
-    if(keycode==KEY_T) { key_ascii = 't'; }
-    if(keycode==KEY_U) { key_ascii = 'u'; }
-    if(keycode==KEY_V) { key_ascii = 'v'; }
-    if(keycode==KEY_W) { key_ascii = 'w'; }
-    if(keycode==KEY_X) { key_ascii = 'x'; }
-    if(keycode==KEY_Y) { key_ascii = 'y'; }
-    if(keycode==KEY_Z) { key_ascii = 'z'; }
-    if(keycode==KEY_1) { key_ascii = '1'; }
-    if(keycode==KEY_2) { key_ascii = '2'; }
-    if(keycode==KEY_3) { key_ascii = '3'; }
-    if(keycode==KEY_4) { key_ascii = '4'; }
-    if(keycode==KEY_5) { key_ascii = '5'; }
-    if(keycode==KEY_6) { key_ascii = '6'; }
-    if(keycode==KEY_7) { key_ascii = '7'; }
-    if(keycode==KEY_8) { key_ascii = '8'; }
-    if(keycode==KEY_9) { key_ascii = '9'; }
-    if(keycode==KEY_0) { key_ascii = '0'; }
-    if(keycode==KEY_SPACE) { key_ascii = ' '; }
-    if(shift==1 && key_ascii>= 'a' && key_ascii<= 'z') {
+    	if(keycode==KEY_A) { key_ascii = 'a'; }
+    	if(keycode==KEY_B) { key_ascii = 'b'; }
+    	if(keycode==KEY_C) { key_ascii = 'c'; }
+    	if(keycode==KEY_D) { key_ascii = 'd'; }
+    	if(keycode==KEY_E) { key_ascii = 'e'; }
+    	if(keycode==KEY_F) { key_ascii = 'f'; }
+    	if(keycode==KEY_G) { key_ascii = 'g'; }
+    	if(keycode==KEY_H) { key_ascii = 'h'; }
+    	if(keycode==KEY_I) { key_ascii = 'i'; }
+    	if(keycode==KEY_J) { key_ascii = 'j'; }
+    	if(keycode==KEY_K) { key_ascii = 'k'; }
+    	if(keycode==KEY_L) { key_ascii = 'l'; }
+    	if(keycode==KEY_M) { key_ascii = 'm'; }
+    	if(keycode==KEY_N) { key_ascii = 'n'; }
+    	if(keycode==KEY_O) { key_ascii = 'o'; }
+    	if(keycode==KEY_P) { key_ascii = 'p'; }
+    	if(keycode==KEY_Q) { key_ascii = 'q'; }
+    	if(keycode==KEY_R) { key_ascii = 'r'; }
+    	if(keycode==KEY_S) { key_ascii = 's'; }
+    	if(keycode==KEY_T) { key_ascii = 't'; }
+    	if(keycode==KEY_U) { key_ascii = 'u'; }
+    	if(keycode==KEY_V) { key_ascii = 'v'; }
+    	if(keycode==KEY_W) { key_ascii = 'w'; }
+    	if(keycode==KEY_X) { key_ascii = 'x'; }
+    	if(keycode==KEY_Y) { key_ascii = 'y'; }
+    	if(keycode==KEY_Z) { key_ascii = 'z'; }
+    	if(keycode==KEY_1) { key_ascii = '1'; }
+    	if(keycode==KEY_2) { key_ascii = '2'; }
+    	if(keycode==KEY_3) { key_ascii = '3'; }
+    	if(keycode==KEY_4) { key_ascii = '4'; }
+    	if(keycode==KEY_5) { key_ascii = '5'; }
+    	if(keycode==KEY_6) { key_ascii = '6'; }
+    	if(keycode==KEY_7) { key_ascii = '7'; }
+    	if(keycode==KEY_8) { key_ascii = '8'; }
+    	if(keycode==KEY_9) { key_ascii = '9'; }
+    	if(keycode==KEY_0) { key_ascii = '0'; }
+    	if(keycode==KEY_SPACE) { key_ascii = ' '; }
+    	if(shift==1 && key_ascii>= 'a' && key_ascii<= 'z') {
             key_ascii = (key_ascii - 32);
         }
 
-		if(keycode<0x81) {
-			debug_irq1(key_ascii, keycode);
-		}
+		key_is_press(keycode, key_ascii);
 
     }
+
+    outb(0x20, 0x20); //EOI
 }
 
 void irq2_handler(void) {
+	p("UHCI IRQ");
+	uhci_clear_irq(USB);
     outb(0x20, 0x20); //EOI
 
 }
@@ -171,7 +187,7 @@ void irq9_handler(void) {
 
 }
 
-void irq10_handler(void) {
+void irq10_handler(void) {  //Sound card IRQ - Intel HD Audio
     outb(0xA0, 0x20);
     outb(0x20, 0x20); //EOI
 
@@ -182,16 +198,33 @@ void irq11_handler(void) {
     outb(0x20, 0x20); //EOI
 }
 
-void irq12_handler(void) {
+void irq12_handler(void) { 
+	mouse_packet[mouse_cycle++] = inb(0x60);
+
+	if(mouse_cycle>=3) {
+		mouse_cycle = 0;
+
+		if(handle_irq12==0) {
+			handle_irq12=1;
+
+    		if ((mouse_packet[0] & 0x80) || (mouse_packet[0] & 0x40)) {
+				handle_irq12=0;
+
+    			outb(0xA0, 0x20);
+    			outb(0x20, 0x20); //EOI
+        		return;
+    		}
+			
+			if(mouse_cursor_visible==1) {
+				handle_mouse_packet(mouse_packet[1], mouse_packet[2], mouse_packet[0], 0);
+			}
+
+			handle_irq12=0;
+		}
+	}
+
     outb(0xA0, 0x20);
     outb(0x20, 0x20); //EOI
-          
-    mouse_bytes[mouse_cycle++] = inb(0x60);
- 
-    if (mouse_cycle == 3) {
-		handle_mouse_packet(mouse_bytes[1], mouse_bytes[2], mouse_bytes[0], 0);
-		mouse_cycle = 0;
-	}
 
 }
 
@@ -202,17 +235,19 @@ void irq13_handler(void) {
 }
 
 void irq14_handler(void) {
-    outb(0xA0, 0x20);
-    outb(0x20, 0x20); //EOI
-
     //acknowledge interrupt
     inb(0x1F7);
+
+    outb(0xA0, 0x20);
+    outb(0x20, 0x20); //EOI
 }
 
 void irq15_handler(void) {
-    outb(0xA0, 0x20);
-    outb(0x20, 0x20); //EOI
-
     //acknowledge interrupt
     inb(0x177);
+
+	ide_secondary_interrupt=1;
+
+    outb(0xA0, 0x20);
+    outb(0x20, 0x20); //EOI
 }
